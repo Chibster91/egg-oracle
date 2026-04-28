cd9262d76f09caa622314ad8c5ee5b87989907da
Chibster91 <jrickard91@gmail.com>
2026-04-28 13:01:59 -0400

    Update sw.js

---
+++ public/sw.js
@@ -0,0 +1,44 @@
+const CACHE = "egg-oracle-v2";
+
+const ASSETS = [
+  "/egg-oracle/",
+  "/egg-oracle/index.html",
+  "/egg-oracle/manifest.webmanifest",
+  "/egg-oracle/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      }).catch(() => {
+        if (event.request.mode === "navigate") {
+          return caches.match("/egg-oracle/index.html");
+        }
+      });
+    })
+  );
+});
